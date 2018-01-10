2c72b5ce4f247b718f60dd93f6699fc6546d60a4
liuwm <790198245@qq.com>
2018-01-10 17:19:39 +0800

    vue+express环境搭建

---
+++ server/app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var fs = require('fs')
+var path = require('path')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+// 访问静态资源
+app.use(express.static(path.resolve(__dirname, '../dist')))
+// 访问单页
+app.get('*', function (req, res) {
+  var html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
+  res.send(html)
+})
+// 监听
+app.listen(8081, function () {
+  console.log('success listen...8081');
+})
