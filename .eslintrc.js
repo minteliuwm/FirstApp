046d9a379c501ad7f73625d87b4fba40fe85fe49
liuwm <790198245@qq.com>
2018-01-12 15:35:35 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,6 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "semi": [2, "always"],
-    "space-before-function-paren": ["error", "never"],
+    "space-before-function-paren": 0
   }
 }
